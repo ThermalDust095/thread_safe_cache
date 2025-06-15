803321bacd6f9cac4f5f9277d6ff8e51890c29ca
Akhil Pendyala <akhil.pendyala@cyware.com>
2025-06-15 21:08:41 +0530

    small change

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -16,7 +16,7 @@ class TestMemorySafeCache:
         key = random.randint(1, 1000)
         value = random.randint(1, 10)
         ttl = random.randint(1, 100)
-        logging.info(f"\n\nAdding Key: {key} and Value: {value}")
+        logging.info(f"\n\nAdding Key: {key} and Value: {value} and ttl: {ttl}")
         cls.cache.put(key, value, ttl)
         logging.info(f"\n\nState of Cache after adding key value to cache: {cls.cache.display()}")
     
